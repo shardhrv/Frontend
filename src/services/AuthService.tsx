e752897ab64c100de70b409604effdd500d114e8
Dhruv <156744940+shardhrv@users.noreply.github.com>
2024-11-16 16:01:23 +0800

    Add logout endpoint

--- src/services/AuthService.tsx
+++ src/services/AuthService.tsx
@@ -5,8 +5,16 @@ import axios from "axios";
 // Ensure cookies are included with every request
 axios.defaults.withCredentials = true;
 
-// Login API
-export const loginAPI = async (username: string, password: string): Promise<UserProfile | null> => {
+export const getUserProfileAPI = async () => {
+  try {
+    const res = await axios.get<UserProfile>("/api/auth/getme");
+    return res.data;
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export const loginAPI = async (username: string, password: string) => {
   try {
     const res = await axios.post<UserProfile>("/api/auth/login", {
       username,
@@ -16,18 +24,16 @@ export const loginAPI = async (username: string, password: string): Promise<User
     return res.data;
   } catch (error) {
     handleError(error);
-    return null;
   }
 };
 
-// Signup API
 export const signupAPI = async (
   username: string,
   firstName: string,
   lastName: string,
   password: string,
   email: string
-): Promise<UserProfile | null> => {
+) => {
   try {
     const res = await axios.post<UserProfile>("/api/auth/signup", {
       username,
@@ -40,22 +46,9 @@ export const signupAPI = async (
     return res.data;
   } catch (error) {
     handleError(error);
-    return null;
   }
 };
 
-// Verify Authentication API
-export const verifyAuthAPI = async (): Promise<UserProfile | null> => {
-  try {
-    const res = await axios.get<UserProfile>("/api/auth/verify");
-    return res.data;
-  } catch (error) {
-    handleError(error);
-    return null;
-  }
-};
-
-// Logout API
 export const logoutAPI = async (): Promise<void> => {
   try {
     await axios.post("/api/auth/logout");
