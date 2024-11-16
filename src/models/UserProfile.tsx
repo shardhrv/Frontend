49cbcaccc135558a187758052cdb5a82aac76644
Dhruv <156744940+shardhrv@users.noreply.github.com>
2024-11-16 19:52:58 +0800

    Update User Profile to be more accurate

--- src/models/UserProfile.tsx
+++ src/models/UserProfile.tsx
@@ -3,22 +3,23 @@ export type UserProfileToken = UserProfile & {
   };  
 
 export type UserProfile = {
-    username: string; 
-    email: string;
-    firstName: string;
-    lastName: string;
-    // Optional fields
-    profileImage?: string; 
-    coverImage?: string; 
-    bio?: string; 
-    link?: string; 
-    dob?: string;
-    country?: string;
-    educationLevel?: string;
-    academicYear?: string;
-    // IDs
-    followers?: string[];
-    following?: string[];
-    likedPosts?: string[];
+  _id: string;  
+  username: string; 
+  email: string;
+  firstName: string;
+  lastName: string;
+  // Optional fields
+  profileImage?: string; 
+  coverImage?: string; 
+  bio?: string; 
+  link?: string; 
+  dob?: string;
+  country?: string;
+  educationLevel?: string;
+  academicYear?: string;
+  // IDs
+  followers?: string[];
+  following?: string[];
+  likedPosts?: string[];
   };
   
