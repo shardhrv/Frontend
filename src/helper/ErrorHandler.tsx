ffbc312d095d3d8664f94fafc04ac7f3e5b41b66
Dhruv <156744940+shardhrv@users.noreply.github.com>
2024-11-16 12:08:40 +0800

    Updated error handling to use axios

--- src/helper/ErrorHandler.tsx
+++ src/helper/ErrorHandler.tsx
@@ -1,23 +1,24 @@
-import { toast } from "react-hot-toast";
+import axios from "axios";
+import toast from "react-hot-toast";
 
-export const handleError = (error: unknown) => {
-  // Check if the error is an instance of Error (thrown by fetch or custom logic)
-  if (error instanceof Error) {
-    const errorMessage = error.message;
-
-    // Parse the error message for custom error structures if needed
-    if (errorMessage.includes("401")) {
+export const handleError = (error: any) => {
+  if (axios.isAxiosError(error)) {
+    var err = error.response;
+    if (Array.isArray(err?.data.errors)) {
+      for (let val of err?.data.errors) {
+        toast.error(val.description);
+      }
+    } else if (typeof err?.data.errors === "object") {
+      for (let e in err?.data.errors) {
+        toast.error(err.data.errors[e][0]);
+      }
+    } else if (err?.data) {
+      toast.error(err.data);
+    } else if (err?.status == 401) {
       toast.error("Please login");
       window.history.pushState({}, "LoginPage", "/login");
-    } else if (errorMessage.includes("403")) {
-      toast.error("You are not authorised to perform this action.");
-    } else if (errorMessage.includes("500")) {
-      toast.error("Internal server error. Please try again later.");
-    } else {
-      toast.error(errorMessage || "An unknown error occurred.");
+    } else if (err) {
+      toast.error(err?.data);
     }
-  } else {
-    // Generic fallback for unknown error structures
-    toast.error("An unknown error occurred. Please try again.");
   }
 };
