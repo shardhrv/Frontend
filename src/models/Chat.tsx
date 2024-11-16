ac2eb1938798bd6c2174fb59b8251529fc6033e1
Dhruv <156744940+shardhrv@users.noreply.github.com>
2024-11-16 19:52:38 +0800

    Update Chat model in frontend to be more accurate

--- src/models/Chat.tsx
+++ src/models/Chat.tsx
@@ -1,11 +1,14 @@
-import { Message } from "./Message"; // Import the Message interface
+import { Message } from "./Message";
 
 export interface Chat {
   _id: string; // MongoDB ObjectId as a string
   participants: {
-    _id: string;
-    name: string;
+    _id: string; 
+    username: string; 
+    firstName: string; 
+    lastName: string; 
     email: string;
+    profileImage: string;
   }[];
   messages: Message[];
   createdAt: string;
