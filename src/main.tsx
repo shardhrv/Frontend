32834c57a19d349d53661cce5ce6c5ab91ee09cd
Dhruv <156744940+shardhrv@users.noreply.github.com>
2024-12-14 00:38:56 +0800

    Removed whitespace form main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
@@ -14,4 +13,3 @@ createRoot(document.getElementById('root')!).render(
     </QueryClientProvider>
   </BrowserRouter>
 );
-
