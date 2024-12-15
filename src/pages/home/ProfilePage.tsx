a87ee4e4f3e69bd9f1982831c4133411e5ab8254
Dhruv <156744940+shardhrv@users.noreply.github.com>
2024-12-15 16:21:05 +0800

    Add sidebar to home and profile pages

--- src/pages/home/ProfilePage.tsx
+++ src/pages/home/ProfilePage.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import MyAppBar from '../../components/layout/AppBar';
+import SideBar from '../../components/layout/SideBar';
 import ProfileInformation from '../../components/cards/ProfileInfoCard';
 
 const ProfilePage: React.FC = () => {
   return (
     <div className="profile-page min-h-screen bg-gray-100">
-      {/* AppBar fixed on top */}
       <MyAppBar />
-      {/* Add top margin to avoid content being under AppBar */}
-      <div className="pt-[64px]">
-        <ProfileInformation />
+      <div className="flex w-screen h-full pt-[56px]">
+        <div className="flex sticky top-[56px] left-0 h-[calc(100vh-56px)]">
+          <SideBar />
+        </div>
+        <div className="flex flex-grow justify-center items-center">
+          <ProfileInformation />
+        </div>
       </div>
     </div>
   );
